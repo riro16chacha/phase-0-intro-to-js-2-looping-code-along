5794a38f997b901861a07eb174da50c4fb5e0555
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-04-03 12:24:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+for (let age=30; age<40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+const names = ["Guadalupe","Ollie","Aki"]
+function writeCards(names,events){
+    for(let i = 0;i<names.length;i++)
+    console.log(`Thank you $names[i], for the wonderful surprise gift!`);
+}
+writeCards(["Guadalupe","Ollie","Aki"], "birthday");
+
+function countDown(positiveNumbers){
+    for(i = parseInt(positiveNumbers);i=0;i--){
+        console.log(i)
+    }
+
+}
+
